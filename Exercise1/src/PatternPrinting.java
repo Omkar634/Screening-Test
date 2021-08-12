2ee2d950e4e8db84d862b83ab1c84cb118d97c8e
omkarnaththakur <omkarnaththakur@omkarnaths-mbp.home>
2021-08-11 21:45:54 -0500

    First Commit

---
+++ Exercise1/src/PatternPrinting.java
@@ -0,0 +1,20 @@
+
+public class PatternPrinting {
+
+	public static void main(String args[])
+
+	{
+		int a, b;
+
+		for (a = 0; a < 6; a++) {
+
+			for (b = 0; b <= a; b++) {
+
+				System.out.print("* ");
+			}
+
+			System.out.println();
+		}
+
+	}
+}
